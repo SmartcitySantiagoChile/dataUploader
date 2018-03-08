c5e916872d08f05e9930e6ca67a1afbcd2a9015f
Felipe Hernández <cephei.1313@gmail.com>
2018-03-08 11:45:32 -0300

    changed values from time period fields

--- uploader/profile.py
+++ uploader/profile.py
@@ -23,7 +23,7 @@ class ProfileFile(DataFile):
         time_period_in_stop_time = row['timePeriodInStopTime']
         if expedition_stop_time == '-':
             expedition_stop_time = "0"
-            time_period_in_stop_time = ""
+            time_period_in_stop_time = -1
         return {
             "path": path,
             "timestamp": timestamp,
@@ -45,10 +45,9 @@ class ProfileFile(DataFile):
             "busCapacity": int(row['busCapacity']),
             "expeditionStopTime": expedition_stop_time,
             "userStopCode": row['userStopCode'],
-            "timePeriodInStartTime": row['timePeriodInStartTime'],
-            "timePeriodInStopTime": time_period_in_stop_time,
-            # TODO: make day_type int when it will convert to number type
-            "dayType": row['dayType'],
+            "timePeriodInStartTime": int(row['timePeriodInStartTime']),
+            "timePeriodInStopTime": int(time_period_in_stop_time),
+            "dayType": int(row['dayType']),
             "busStation": int(row['busStation']),
             "transactions": int(row['transactions']),
             "halfHourInStartTime": int(row['halfHourInStartTime']),
