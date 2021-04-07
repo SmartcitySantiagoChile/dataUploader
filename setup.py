d9fe68e4742734e7c49d535033e53a37a31f2952
Felipinbombin <cephei.1313@gmail.com>
2021-04-07 13:34:05 -0400

    added setup.py to code

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='data-uploader',
+    version='v1.0.0',
+    author='ADATRAP',
+    author_email='cephei.1313@gmail.com',
+    description='library to manage ADATRAP data in elasticsearch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/SmartcitySantiagoChile/dataUploader',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0'
+)
