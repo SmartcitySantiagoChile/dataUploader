0281133cb1c3f48ad7860015d4fe66a65af6c6d8
Felipinbombin <cephei.1313@gmail.com>
2021-05-12 12:29:37 -0400

    update version code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='data-uploader',
-    version='2.0.2',
+    version='2.0.3',
     author='ADATRAP',
     author_email='cephei.1313@gmail.com',
     description='library to manage ADATRAP data in elasticsearch',
