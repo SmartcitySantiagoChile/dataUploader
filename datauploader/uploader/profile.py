aa219985bf761826ddebebe2414d7433c4fb1a70
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-09-23 18:01:41 -0300

    Add Profile field names tests

--- datauploader/uploader/profile.py
+++ datauploader/uploader/profile.py
@@ -14,34 +14,40 @@ class ProfileFile(DataFile):
                            'TiempoPrimeraTrx', 'TiempoGPSMasCercano', 'Tiempo', 'nSubidasTmp',
                            'ParaderoUsuario', 'PeriodoTSExpedicion', 'PeriodoTSParada', 'TipoDia', 'ZP',
                            'DeltaTrxs', 'MHSalida', 'MHPasada', 'ExpedicionConProblema',
-                           'expandedEvasionBoarding', 'expandedEvasionAlighting',
-                           'expandedBoardingPlusExpandedEvasionBoarding',
-                           'expandedAlightingPlusExpandedEvasionAlighting', 'loadProfileWithEvasion',
-                           'boardingWithAlighting', 'evasionPercent', 'evasionType', 'uniformDistributionMethod',
+                           'subidas_evadidas', 'bajadas_evadidas',
+                           'subidas_corregidas',
+                           'bajadas_corregidas', 'carga_corregida',
+                           'subidas_conbajada', '%evasion', 'tipo_evasion', 'uniforme',
                            'pax-km_tramo', 'pax-km_corregido_tramo', 'plazas-km_tramo'
                            ]
+
+    a = ["Operador", "ServicioSentido", "ServicioUsuario", "Patente", "Paradero", "NombreParada", "Hini", "Hfin",
+         "Cumplimiento", "Correlativo", "idExpedicion", "DistEnRuta", "#Subidas", "#SubidasLejanas", "Subidastotal",
+         "SubidasExpandidas", "#Bajadas", "#BajadasLejanas", "Bajadastotal", "BajadasExpandidas", "Carga", "Capacidad",
+         "TiempoGPSInterpolado", "TiempoPrimeraTrx", "TiempoGPSMasCercano", "Tiempo", "nSubidasTmp", "ParaderoUsuario",
+         "PeriodoTSExpedicion", "PeriodoTSParada", "TipoDia", "ZP", "DeltaTrxs", "MHSalida", "MHPasada",
+         "ExpedicionConProblema", "subidas_evadidas", "bajadas_evadidas", "subidas_corregidas", "bajadas_corregidas",
+         "carga_corregida", "subidas_conbajada", "%evasion", "tipo_evasion", "uniforme", "pax-km_tramo",
+         "pax-km_corregido_tramo", "plazas-km_tramo"]
 
     def row_parser(self, row, path, timestamp):
         expedition_stop_time = row['Tiempo']
         time_period_in_stop_time = row['PeriodoTSParada']
         half_hour_in_stop_time = -1 if row['MHPasada'] is None else row['MHPasada']
-        expanded_evasion_boarding = 0 if row['expandedEvasionBoarding'] is None else row['expandedEvasionBoarding']
-        expanded_evasion_alighting = 0 if row['expandedEvasionAlighting'] is None else row['expandedEvasionAlighting']
-        expanded_boarding_plus_expanded_evasion_boarding = 0 if row[
-                                                                    'expandedBoardingPlusExpandedEvasionBoarding'] is None else \
-            row['expandedBoardingPlusExpandedEvasionBoarding']
-        expanded_alighting_plus_expanded_evasion_alighting = 0 if row[
-                                                                      'expandedAlightingPlusExpandedEvasionAlighting'] is None else \
-            row['expandedAlightingPlusExpandedEvasionAlighting']
-        load_profile_with_evasion = 0 if row['loadProfileWithEvasion'] is None else row['loadProfileWithEvasion']
-        boarding_with_alighting = 0 if row['boardingWithAlighting'] is None else row['boardingWithAlighting']
+        expanded_evasion_boarding = 0 if row['subidas_evadidas'] is None else row['subidas_evadidas']
+        expanded_evasion_alighting = 0 if row['bajadas_evadidas'] is None else row['bajadas_evadidas']
+        expanded_boarding_plus_expanded_evasion_boarding = 0 if row['subidas_corregidas'] is None else \
+            row['subidas_corregidas']
+        expanded_alighting_plus_expanded_evasion_alighting = 0 if row['bajadas_corregidas'] is None else \
+            row['bajadas_corregidas']
+        load_profile_with_evasion = 0 if row['carga_corregida'] is None else row['carga_corregida']
+        boarding_with_alighting = 0 if row['subidas_conbajada'] is None else row['subidas_conbajada']
         if expedition_stop_time == '-':
             time_period_in_stop_time = -1
             half_hour_in_stop_time = -1
-        evasion_percent = 0 if row['evasionPercent'] is None else float(row['evasionPercent'])
-        evasion_type = -1 if row['evasionType'] is None else int(row['evasionType'])
-        uniform_distribution_method = -1 if row['uniformDistributionMethod'] is None else int(
-            row['uniformDistributionMethod'])
+        evasion_percent = 0 if row['%evasion'] is None else float(row['%evasion'])
+        evasion_type = -1 if row['tipo_evasion'] is None else int(row['tipo_evasion'])
+        uniform_distribution_method = -1 if row['uniforme'] is None else int(row['uniforme'])
         pax_km_section = 0 if row['pax-km_tramo'] is None or row['pax-km_tramo'] == '-' else float(row['pax-km_tramo'])
         pax_with_evasion_km_section = 0 if row['pax-km_corregido_tramo'] is None or row[
             'pax-km_corregido_tramo'] == '-' else float(row['pax-km_corregido_tramo'])
