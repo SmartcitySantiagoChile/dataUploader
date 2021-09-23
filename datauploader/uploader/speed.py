4fedb821fc62d45f3f6e4a1633fcf846aca32773
Bastianleaf <bastian.martinez@ing.uchile.cl>
2021-09-23 18:22:14 -0300

    Add Speed field names tests

--- datauploader/uploader/speed.py
+++ datauploader/uploader/speed.py
@@ -6,26 +6,27 @@ class SpeedFile(DataFile):
 
     def __init__(self, datafile):
         DataFile.__init__(self, datafile)
-        self.fieldnames = ['authRouteCode', 'section', 'date', 'periodId', 'dayType', 'totalDistance',
-                           'totalTime', 'speed', 'nObs', 'nInvalidObs', 'operator', 'userRouteCode', 'isEndSection']
+        self.fieldnames = ['Route', 'IdSection', 'Date', 'Period', 'DayType', 'TotalDistance',
+                           'TotalTime', 'Speed', 'Observations', 'InvalidObservations', 'Operator', 'RouteUser',
+                           'IsEndSection']
 
     def row_parser(self, row, path, timestamp):
-        merged = str(row['authRouteCode'] + '-' + row['section'] + '-' + row['periodId'])
+        merged = str(row['Route'] + '-' + row['IdSection'] + '-' + row['Period'])
         return {
             "path": path,
             "timestamp": timestamp,
             "merged": merged,
-            "authRouteCode": row['authRouteCode'],
-            "userRouteCode": row['userRouteCode'],
-            "operator": int(row['operator']),
-            "section": int(row['section']),
-            "date": row['date'],
-            "periodId": int(row['periodId']),
-            "dayType": int(row['dayType']),
-            "totalDistance": float(row['totalDistance']),
-            "totalTime": float(row['totalTime']),
-            "speed": float(row['speed']),
-            "nObs": int(row['nObs']),
-            "nInvalidObs": int(row['nInvalidObs']),
-            "isEndSection": int(row['isEndSection'])
+            "authRouteCode": row['Route'],
+            "userRouteCode": row['RouteUser'],
+            "operator": int(row['Operator']),
+            "section": int(row['IdSection']),
+            "date": row['Date'],
+            "periodId": int(row['Period']),
+            "dayType": int(row['DayType']),
+            "totalDistance": float(row['TotalDistance']),
+            "totalTime": float(row['TotalTime']),
+            "speed": float(row['Speed']),
+            "nObs": int(row['Observations']),
+            "nInvalidObs": int(row['InvalidObservations']),
+            "isEndSection": int(row['IsEndSection'])
         }
