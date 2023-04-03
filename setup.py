5d9cb7f9d4e98bb179521eff27a8945a66ce3f86
cephe <cephei.1313@gmail.com>
2023-04-03 11:40:09 +0100

    add dependencies to setup.py file

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'r', encoding='utf-8') as fh:
+    install_requires = fh.read().splitlines()
+
 setuptools.setup(
     name='data-uploader',
-    version='2.0.29',
+    version='2.0.30',
     author='ADATRAP',
     author_email='cephei.1313@gmail.com',
     description='library to manage ADATRAP data in elasticsearch',
@@ -22,5 +25,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0'
+    python_requires='>=3.0',
+    install_requires=install_requires
 )
